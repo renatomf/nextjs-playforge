1eff4fae36f33d1f35b4f4933bbe5b53bb7521ed
Renato Marques <renatomardev@gmail.com>
2026-09-09 17:51:11 -0300

    feat: add clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { UserButton } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs/server"
 import Image from "next/image"
 
 import {
@@ -8,21 +10,26 @@ import {
   EmptyTitle,
 } from "@/components/ui/empty"
 
-export default function Page() {
+export default async function Page() {
+  await auth.protect()
+
   return (
-    <div className="flex min-h-svh flex-col">
-      <Empty>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Empty className="flex-none">
         <EmptyHeader>
           <EmptyMedia>
             <Image src="/logo.svg" alt="Logo" width={48} height={48} />
           </EmptyMedia>
-          <EmptyTitle className="text-2xl">What should we build today?</EmptyTitle>
+          <EmptyTitle className="text-2xl">
+            What should we build today?
+          </EmptyTitle>
           <EmptyDescription>
             Build your own racers, shooters, puzzles and whole worlds using your
             own words. If you can describe it, you can play it.
           </EmptyDescription>
         </EmptyHeader>
       </Empty>
+      <UserButton />
     </div>
   )
 }
