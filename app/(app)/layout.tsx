d97ad3a5012b9fbdcf8e9db4fdea80fad80cdfd9
Renato Marques <renatomardev@gmail.com>
2026-09-10 00:35:26 -0300

    feat: add base sidebar

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,11 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+
+export default function AppLayout({ children }: LayoutProps<"/">) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>{children}</SidebarInset>
+    </SidebarProvider>
+  )
+}
